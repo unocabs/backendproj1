3747ac44f8fb5eadf83c21c552a9ecadd1302b70
unocabs <rgiancabrera@gmail.com>
2023-04-07 06:17:02 +0800

    Before Jowel Edit

--- backendproj1/src/main/java/com/rosgian/backendproj1/service/CoffeeService.java
+++ backendproj1/src/main/java/com/rosgian/backendproj1/service/CoffeeService.java
@@ -2,17 +2,51 @@ package com.rosgian.backendproj1.service;
 
 
 import com.rosgian.backendproj1.DTO.CoffeeDTO;
+import com.rosgian.backendproj1.entity.CoffeeEntity;
 import com.rosgian.backendproj1.model.CoffeeModel;
+import com.rosgian.backendproj1.repository.CoffeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class CoffeeService {
 
+    private final CoffeeRepository coffeeRepository;
+
     public CoffeeDTO postCoffee(CoffeeModel coffeeModel) {
-        return CoffeeDTO.builder()
+        coffeeRepository.save(CoffeeEntity.builder()
+                .coffeeId(UUID.randomUUID())
                 .coffeeName(coffeeModel.getCoffeeName())
                 .location(coffeeModel.getLocation())
                 .price(coffeeModel.getPrice())
-                .build();
+                .build());
+         /*CoffeeDTO.builder()
+                .coffeeName(coffeeModel.getCoffeeName())
+                .location(coffeeModel.getLocation())
+                .price(coffeeModel.getPrice())
+                .build();*/
+
+        return null;
     }
+/*
+    public List<BlogDTO> addBlog(BlogRequest newBlog) {
+
+        // Save new blog to database
+        blogRepository.save(BlogEntity
+                .builder()
+                .blogId(UUID.randomUUID())
+                .blogName(newBlog.getBlogName())
+                .blogAuthor(newBlog.getBlogAuthor())
+                .imageLink(null)
+                .description(newBlog.getDescription())
+                .createdDate(dateTimeUtil.currentDate())
+                .modifiedDate(dateTimeUtil.currentDate())
+                .build());
+
+        return getAllBlogs();
+    }*/
+
 }
